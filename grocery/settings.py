69f68a10dcfae7def40a2d5ac1d69bca6894b84a
Noorul <noorulahsanpp@gmail.com>
2021-02-07 13:31:04 +0530

    code modified 1.1

--- grocery/settings.py
+++ grocery/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = '8!*i8y09_jb443#kjl#t9skc2(hst1n-lngtiuju882&(--*36'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://noorulahsan.pythonanywhere.com/']
 
 
 # Application definition
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+try:
+    from local_settings import *
+except ImportError:
+    print("Looks like no local file. ")
